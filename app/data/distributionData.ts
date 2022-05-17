386f61f372fd07fbf92951216ccb4311c6472fc0
Karntino Areros <karntino.c.areros@gmail.com>
2022-05-17 23:42:09 +0700

    removed an eslint rule comment

--- app/data/distributionData.ts
+++ app/data/distributionData.ts
@@ -1,6 +1,5 @@
 import generated from './generated';
 
-// eslint-disable-next-line no-undef
 const distributionData: GeoJSON.FeatureCollection = {
   type: 'FeatureCollection',
   features: generated,
